46ffe16313d9cdaf8a88f0821a24cc801486e74c
Jason <jljefferson102@gmail.com>
2021-03-15 19:50:28 -0400

    dynamic generation of cards

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const createCards = () => {
+  var node = document.getElementById('slider');
+  let slideCount = node.dataset.slides;
+  let cards = '';
+  let card =
+      '<div class='+'"'+'card'+'"'+ 'data-target='+'"'+'card'+'"'+'>'+
+      '<div class='+'"'+'card-image'+'"'+'></div>'+
+      '<div class='+'"'+'card-body'+'"'+'>' +
+      '<div class='+'"'+'wrapper'+'"'+'>'+
+      '<h3>We are Humans</h3>'+
+      '<p class='+'"'+'subheader'+'"'+'>What will you find here</p>'+
+      '<p>LOERM IPSUM.</p>'+
+      '<a href='+'"'+'https://gohenry.com/uk'+'"'+ 'target='+'"'+'_blank'+'"'+'>Learn More</a>'+
+      '</div>'+
+      '</div>'+
+      '</div>';
+
+  for (i = 0; i < slideCount; i++) {
+    cards = cards + card;
+  };
+  node.innerHTML = cards;
+}
+
+createCards();
+
 const slider = document.querySelector("[data-target='card-slider']");
 const card = slider.querySelector("[data-target='card']");
 const leftButton = document.querySelector("[data-action='moveLeft']");
@@ -11,8 +36,8 @@ const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
 const cardCount = slider.querySelectorAll("[data-target='card']").length;
 
 let offset = 0;
-const maxX = -((cardCount / 3) * sliderWidth + 
-               (cardMarginRight * (cardCount / 3)) - 
+const maxX = -((cardCount / 3) * sliderWidth +
+               (cardMarginRight * (cardCount / 3)) -
                sliderWidth - cardMarginRight);
 
 leftButton.addEventListener("click", function() {
@@ -21,7 +46,7 @@ leftButton.addEventListener("click", function() {
     slider.style.transform = `translateX(${offset}px)`;
     }
 })
-  
+
 rightButton.addEventListener("click", function() {
   if (offset !== maxX) {
     offset -= cardWidth + cardMarginRight;
